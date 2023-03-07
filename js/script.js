564d64be93a38548928904706d3eee2abfbfcf6f
yutaokkots <yuta.okkotsu@gmail.com>
2023-03-07 11:39:17 -0700

    introduced several constants, simplified some render functions to be compatible with master array

--- js/script.js
+++ js/script.js
@@ -25,6 +25,9 @@ let currentBottomCardType;    //this variable holds the information about what c
 // holds master information about 
 // 1) card is face or back 
 
+//should be a key:value pairing of all card information and their DOM objects, used for reference
+let allCardObjectKeyValue = {};
+
 let allCardDivElements = {};
 
 
@@ -32,6 +35,42 @@ let allCardRefElements = {};
 
 
 let cardObjectKeyValue = {}
+
+
+const boardArray = [
+    [],                     //tableau-0     [0]
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],                     //tableau-7     [6]
+    [],                     //foundation-1  [7]
+    [],                     //foundation-2  [8]
+    [],                     //foundation-3  [9]
+    [],                     //foundation-4  [10]
+    [],                     // shown        [11] 
+    [],                     // stock        [12] 
+    []                      // waste        [13] 
+]
+const boardArrayKey = {
+0: "tableau-0",
+1: "tableau-1",
+2: "tableau-2",
+3: "tableau-3",
+4: "tableau-4",
+5: "tableau-5",
+6: "tableau-6",
+7: "foundation-1",
+8: "foundation-2",
+9: "foundation-3",
+10:"foundation-4",
+11:"shown",
+12:"stockCards",
+13:"waste"
+}
+
+
 
 
 
@@ -197,33 +236,7 @@ mainElement.addEventListener('dragend', (evt) => {
 
 /*----- functions -----*/
 
-const boardArray = [
-    [],         //tableau-0 [0]
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],     //tableau-7 [6]
-    [],                    //foundation-0 [7]
-    [],
-    [],
-    [],                    //foundation-4 [10]
-    [],                     // shown     [13] 11
-    [],                     // stock     [11] 12
-    []                      // waste     [12] 13
-]
-
-
-
-const updateDataObject = () => {
-    for (let keys in allCardDivElements){
-        let location = document.querySelector(`${keys}`);
-        allCardDivElements[keys].forEach((element) => {
-            location.append(element);
-        });
-    } 
-}
+//
 
 
 // const revealCard = (topCard) => {
@@ -270,7 +283,6 @@ const moveCard = (topCard, bottomCard) => {
 
 
 
-function flipCard(){};           // changes the variable state of each card to show face
 function checkPlacement(){};    // checks to see if dragged card is able to be placed at location
 
 
@@ -320,15 +332,50 @@ function resetStockFromWaste(){
 
 /* --- shuffle the cards --- */
 const randomShuffle = (cardArr) => {
-    let newDeck = [];
+    let initialDeck = [...cardArr];
+    let newDeck = []
     for (let i = 52; i > 0; i--) {
         let n = Math.floor(Math.random() * i);
-        newDeck.push(...cardArr.splice(n, 1));
+        newDeck.push(...initialDeck.splice(n, 1));
     };     
     return newDeck;
 };
 
-currentDeck = randomShuffle(CARD_LIST)
+/* --- flip  the cards --- */
+const flipCard = (cardObject) => {
+    let currentState = cardObject.dataset.inplay
+    cardObject.dataset.inplay = currentState * -1
+
+}
+
+
+// re-doing function 3/7:
+// makeDivElementWithCard creates a new div that contains dataset type attributes (e.g. "data-name" within HTML)
+const makeCardObjects = (cardDeckShuffledArray) => {
+    cardDeckShuffledArray.forEach((cardId) => {
+        let newDiv= document.createElement('div')
+        let cardUrl = CARD_LIBRARY[cardId]["imgBack"]
+        newDiv.innerHTML = `<img data-card='${cardId}' data-inplay='-1' src=${cardUrl}>`
+        newDiv.dataset.card=`${cardId}`
+        newDiv.dataset.inplay=`-1`
+        allCardObjectKeyValue[cardId] = newDiv;
+
+    })
+}
+
+
+
+
+const makeCardO = (cardIdentity, cardUpOrDown) => {
+    let newDiv= document.createElement('div')
+    // uses constant CARD_LIBRARY to get the image source
+    let cardUrl = cardUpOrDown > 0 ? CARD_LIBRARY[cardIdentity]["img"] : CARD_LIBRARY[cardIdentity]["imgBack"];
+    newDiv.innerHTML = `<img data-card='${cardIdentity}' data-inplay='${cardUpOrDown}' src=${cardUrl}>`
+    newDiv.dataset.card=`${cardIdentity}`
+    newDiv.dataset.inplay=`${cardUpOrDown}`
+    newDiv.draggable = 'true';                  //is this needed?
+    return newDiv
+}
 
 
 // makeDivElementWithCard creates a new div that contains dataset type attributes (e.g. "data-name" within HTML)
@@ -341,6 +388,16 @@ const makeDivElementWithCard = (cardIdentity, cardUpOrDown) => {
     newDiv.dataset.inplay=`${cardUpOrDown}`
     newDiv.draggable = 'true';                  //is this needed?
     return newDiv
+}
+
+
+const updateDataObject = () => {
+    for (let keys in allCardDivElements){
+        let location = document.querySelector(`${keys}`);
+        allCardDivElements[keys].forEach((element) => {
+            location.append(element);
+        });
+    } 
 }
 
 
@@ -358,24 +415,6 @@ const makeDivElementWithCard = (cardIdentity, cardUpOrDown) => {
 //         })
 //     }
 // }
-
-const renderTableau = () => {
-    // create a new div for each column in tableau
-    for (let n=1;n<8;n++) {
-        let tableauKey = `#tableau-${n-1}`
-        allCardDivElements[tableauKey] = []
-        allCardRefElements[tableauKey] = []
-        startingTableau[n].forEach((isCardUpOrDown) => {
-            let nextCard = dealCardFromDeck()
-            let nextDiv = makeDivElementWithCard(nextCard, isCardUpOrDown)
-            allCardDivElements[tableauKey].push(nextDiv)
-            allCardRefElements[tableauKey].push(nextCard)
-            cardObjectKeyValue[nextCard] = nextDiv
-            boardArray[n-1].push(nextCard)
-
-        })
-    }
-}
 
 
 
@@ -397,19 +436,63 @@ const dealCardFromDeck = () => {
 // break up render() into smaller renderXxxx(), because it cam get bloated
 // Render function should transfer all states to user interface. 
 
+// random-shuffles initial deck 
+const renderDeck = () => {
+    currentDeck = randomShuffle(CARD_LIST)
+    makeCardObjects(currentDeck)
+}
+
 const renderStock = () => {
-    allCardDivElements["#stock"] = []
-    allCardRefElements["#stock"] = []
     while (currentDeck.length > 0){
-        let nextCard = dealCardFromDeck();
-        let nextDiv = makeDivElementWithCard(nextCard, -1);
-        //stockPile.appendChild(nextDiv);
-        allCardDivElements["#stock"].push(nextDiv);
-        allCardRefElements["#stock"].push(nextCard);
-        cardObjectKeyValue[nextCard] = nextDiv
+        let nextCard = currentDeck.pop()
         boardArray[12].push(nextCard)
     }
+    console.log(boardArray)
 }
+
+    // allCardDivElements["#stock"] = []
+    // allCardRefElements["#stock"] = []
+    // while (currentDeck.length > 0){
+    //     let nextCard = dealCardFromDeck();
+    //     let nextDiv = makeDivElementWithCard(nextCard, -1);
+    //     //stockPile.appendChild(nextDiv);
+    //     allCardDivElements["#stock"].push(nextDiv);
+    //     allCardRefElements["#stock"].push(nextCard);
+    //     cardObjectKeyValue[nextCard] = nextDiv
+        
+const renderTableau = () => {
+    // create a new div for each column in tableau
+    for (let n=0;n<7;n++) {
+        // let tableauKey = `#tableau-${n}`
+
+        // allCardObjectKeyValue // a 1-D key-value pair object
+        
+        // allCardDivElements[tableauKey] = []
+        // allCardRefElements[tableauKey] = []
+
+        startingTableau[n+1].forEach((isCardUpOrDown) => {
+
+            let card = boardArray[12].pop()
+            if (isCardUpOrDown == -1){
+                flipCard(allCardObjectKeyValue[card])
+            }
+            boardArray[n].push(card)
+            
+            // let nextCard = dealCardFromDeck()
+            // let nextDiv = makeDivElementWithCard(nextCard, isCardUpOrDown)
+            // allCardDivElements[tableauKey].push(nextDiv)
+            // allCardRefElements[tableauKey].push(nextCard)
+            // cardObjectKeyValue[nextCard] = nextDiv
+            // boardArray[n-1].push(nextCard)
+
+        })
+    }
+}
+    
+    
+
+
+
 
 //WORK ON THIS FOR 3/6
 
@@ -423,6 +506,8 @@ const renderStock = () => {
 
 
 const renderWaste = () => {
+
+
     //let stockPile = document.querySelector(`#stock`)                // later remove to different function
     allCardDivElements["#waste"] = []
     allCardRefElements["#waste"] = []
@@ -437,6 +522,7 @@ const renderFoundation = () => {
 }
 
 const updateBoard = () => {
+    console.log(allCardDivElements)
     for (let keys in allCardDivElements){
         let location = document.querySelector(`${keys}`);
         allCardDivElements[keys].forEach((element) => {
@@ -446,10 +532,27 @@ const updateBoard = () => {
 }
 
 
-function render(renderT, renderF, renderS, renderW, updateB, ){
-    setTimeout(renderT, 10);
-    setTimeout(renderF, 15);
-    setTimeout(renderS, 20);
+
+// for (let [index, value] of anArray.entries()){}
+
+const assignBoardPlacement = () => {
+    for (const [indexCol, cardList] of boardArray.entries) {
+        for (const [indexRow, card] of cardList.entries){
+            
+        }
+
+    }
+
+}
+
+
+
+
+function render(renderT, renderF, renderS, renderW, updateB){
+    renderDeck();
+    setTimeout(renderS, 5);
+    setTimeout(renderT, 15);
+    setTimeout(renderF, 20);
     setTimeout(renderW, 25);
     setTimeout(updateB, 200);
 
@@ -457,9 +560,8 @@ function render(renderT, renderF, renderS, renderW, updateB, ){
 
 
 function init(){
-    render(renderTableau, renderFoundation, renderStock, renderWaste, updateBoard, );
+    render(renderTableau, renderFoundation, renderStock, renderWaste, updateBoard);
     console.log('This is the init function');
 }
 
 init();
-
